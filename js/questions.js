c9d878cbaad4af6335ea1de717838ced5b74fdb4
Rkhyg <126140590+ItsHanz@users.noreply.github.com>
2023-04-03 13:05:53 +0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,93 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Apa ibukota Riau?",
+    answer: "Pekanbaru",
+    options: ["Batam", "Pekanbaru", "Belakang Padang"],
+  },
+  {
+    numb: 2,
+    question: "Apa rumah adat Provinsi Sumatera Barat?",
+    answer: "Gadang",
+    options: ["Gadang", "Rumah Panggung", "Joglo"],
+  },
+  {
+    numb: 3,
+    question: "Julukan kota Bogor adalah?",
+    answer: "Kota Hujan",
+    options: ["Kota Banjir", "Kota Panas", "Kota Hujan"],
+  },
+  {
+    numb: 4,
+    question: "Kapan Hari Lahir Pancasila diperingati?",
+    answer: "1 Juni",
+    options: ["1 Juni", "2 Juni", "3 Juni"],
+  },
+  {
+    numb: 5,
+    question: "Kapan Hari Pendidikan Nasional diperingati?",
+    answer: "2 Mei",
+    options: ["3 Mei", "1 Mei", "2 Mei"],
+  },
+  {
+    numb: 6,
+    question: "Sungai terpanjang di dunia adalah?",
+    answer: "Sungai Nill",
+    options: ["Sungai Kapuas", "Sungai Nill", "Sungai Eufrat"],
+  },
+  {
+    numb: 7,
+    question: "WHO merupakan singkatan dari?",
+    answer: "World Health Organization",
+    options: ["World Health Orientation", "Why Health Organization", "World Health Organization"],
+  },
+  {
+    numb: 8,
+    question: "Hewan yang disebut sebagai raja hutan?",
+    answer: "Singa",
+    options: ["Singa", "Harimau", "Macan"],
+  },
+  {
+    numb: 9,
+    question: "Mata uang Korea Selatan adalah?",
+    answer: "Won",
+    options: ["Won", "Yen", "Rupee"],
+  },
+  {
+    numb: 10,
+    question: "Presiden RI ke-3 adalah?",
+    answer: "Bj Habibie",
+    options: ["Soeharto", "Bj Habibie", "Megawati"],
+  },
+  {
+    numb: 11,
+    question: "Apa saja warna primer?",
+    answer: "Merah, Kuning, Biru",
+    options: ["Hitam, Putih, Merah", "Kuning, Biru, Hijau", "Merah, Kuning, Biru"],
+  },
+  {
+    numb: 12,
+    question: "Di manakah lokasi Gunung Fuji?",
+    answer: "Jepang",
+    options: ["Jepang", "Korea", "Australia"],
+  },
+  {
+    numb: 13,
+    question: "Apa mata uang Jepang?",
+    answer: "Yen",
+    options: ["Won", "Yen", "Dollar"],
+  },
+  {
+    numb: 14,
+    question: "Ibukota Malaysia adalah",
+    answer: "Kuala Lumpur",
+    options: ["Yala", "Perak", "Kuala Lumpur"],
+  },
+  {
+    numb: 15,
+    question: "Rendang merupakan masakan dari daerah?",
+    answer: "Sumatra Barat",
+    options: ["Palembang", "Sumatra Barat", "Yogyakarta"],
+  },
+];
